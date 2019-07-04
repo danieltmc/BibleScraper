9c700e252f7d078af36b3c83111ab7b5900140db
danieltmc <me@danieltmc.com>
2019-07-03 23:51:55 -0400

    Added parsing for web pages

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ base_url = "https://www.biblegateway.com/passage/?search="
 
 book = str(input("Please enter the book of the Bible to scrape"))
 translation = str(input("Please enter the translation to use")).upper()
-chapter = 1
+chapter_num = 1
+verse_num = 1
 
 if not os.path.exists("Bible"):
     os.mkdir("Bible")
@@ -16,6 +17,11 @@ if not os.path.exists("Bible/" + translation):
 if not os.path.exists("Bible/" + translation + "/" + book)
 	os.mkdir("Bible/" + translation + "/" + book)
 
-full_url = base_url + book + "+" str(chapter) + "&version=" + translation
+full_url = base_url + book + "+" + str(chapter_num) + "&version=" + translation
 
 page = requests.get(full_url)
+soup = BeautifulSoup(page.text, "html.parser")
+
+verse = soup.find(class_="text" + book + "-" + str(chapter_num) + "-" + str(verse_num))
+verse_list = soup.find(class_="BodyText")
+verse_list_contents = verse_list.find_all("a")
